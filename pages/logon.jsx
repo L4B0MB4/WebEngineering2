3ae75158c6c6043cabd5b91ec58936327e523d57
L4B0MB4 <metalfan65@gmail.com>
2018-03-20 20:47:27 +0100

    Passport implementation

---
+++ pages/logon.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class logon extends Component {
+  render() {
+    return (
+      <form action="/login" method="post">
+        <div>
+          <label>Username:</label>
+          <input type="text" name="username" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" value="Log In" />
+        </div>
+      </form>
+    );
+  }
+}
