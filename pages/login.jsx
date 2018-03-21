219fe0aebfb85963698966d197715b37f3f51bf2
tinometzger <tino.metzger97@gmx.de>
2018-03-21 11:12:45 +0100

    Login page designs

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,41 +1,41 @@
 import React, { Component } from "react";
 import OwnHeader from "../components/Header.jsx";
-
+import { Grid, Image } from 'semantic-ui-react';
 
 class Login extends Component {
-
     render() {
         return (
             <div>
             <OwnHeader/>
-                <div className="row">
-                    <div className="three wide column">
-                        <img src="/assets/images/wireframe/image.png" className="ui image" />
-                    </div>
-                    <div className="ten wide column">
-                        <form className="ui form">
-                            <div className="field">
-                                <label>E-Mail-Adresse</label>
-                                <div className="ui input">
-                                    <input type="text" placeholder="E-Mail" />
+                <Grid celled>
+                    <Grid.Row>
+                        <Grid.Column width={3}>
+                            <Image src="/static/picture.jpg" />
+                        </Grid.Column>
+                        <Grid.Column width={10}>
+                            <form className="ui form">
+                                <div className="field">
+                                    <label>E-Mail-Adresse</label>
+                                    <div className="ui input">
+                                        <input type="text" placeholder="E-Mail" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="field">
-                                <label>Passwort</label>
-                                <div className="ui input">
-                                    <input type="text" placeholder="Passwort" />
+                                <div className="field">
+                                    <label>Passwort</label>
+                                    <div className="ui input">
+                                        <input type="password" placeholder="Passwort" />
+                                    </div>
                                 </div>
-                            </div>
-                            <button className="ui button" type="submit">Login</button>
-                        </form>
-                    </div>
-                    <div className="three wide column">
-                        <img src="/assets/images/wireframe/image.png" className="ui image" />
-                    </div>
-                </div>
+                                <button className="ui button" type="submit">Login</button>
+                            </form>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Image src="/static/picture.jpg" />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </div>
         );
     }
 }
-
 export default Login;
