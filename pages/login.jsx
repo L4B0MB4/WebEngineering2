b84d74fa11ff1f26eaf7329047328b954ebb0891
tinometzger <tino.metzger97@gmx.de>
2018-03-21 13:52:36 +0100

    Merge branch 'master' of https://github.com/L4B0MB4/WebEngineering2 into 420Branch
    # Conflicts:
    #	pages/login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import OwnHeader from "../components/Header.jsx";
 import { Grid, Image } from 'semantic-ui-react';
 
@@ -35,32 +34,7 @@ class Login extends Component {
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
-=======
-import Layout from "../components/layout.jsx";
-
-class Login extends Component {
-
-    render() {
-        return (
-            <div>
-                <Layout>
-                    <form action={"/api/user/login"} method={"POST"}>
-                        <label htmlFor={"name"}>Name: </label>
-                        <input type="text" name="name" id={"name"} placeholder={"Name"} ref={"name"} />
-
-                        <label htmlFor={"password"}>Passwort: </label>
-                        <input type="text" name="password" id={"password"} placeholder={"Passwort"} ref={"password"} />
-
-                        <button type="submit" value="Login" />
-                    </form>
-                </Layout>
->>>>>>> 0dd4f948ed654c381aa877d829525bdb97c546c9
             </div>
         );
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 0dd4f948ed654c381aa877d829525bdb97c546c9
-export default Login;
