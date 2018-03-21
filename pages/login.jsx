4fe7cc498750e219dc350f7620139bb72b377c06
tinometzger <tino.metzger97@gmx.de>
2018-03-21 10:49:24 +0100

    First login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,39 +1,39 @@
 import React, { Component } from "react";
-import Layout from "../components/layout.jsx";
+import OwnHeader from "../components/Header.jsx";
 
 
 class Login extends Component {
 
     render() {
         return (
-            <Layout>
             <div>
-                <div className={"row"}>
-                    <h1 className="ui header">goldigger.io Login</h1>
-                </div>
-                <div className={"row"}>
-                    <div className={"three wide column"}>
-                        <img className="ui small image" src="./de4022dd39fa43041188aa8e937b6153.jpg"/>
+            <OwnHeader/>
+                <div class="row">
+                    <div class="three wide column">
+                        <img src="/assets/images/wireframe/image.png" class="ui image" />
                     </div>
-                    <div className={"ten wide column"}>
+                    <div class="ten wide column">
                         <form className="ui form">
                             <div className="field">
                                 <label>E-Mail-Adresse</label>
-                                <input type="text" name="first-name" placeholder="email"/>
+                                <div class="ui input">
+                                    <input type="text" placeholder="E-Mail" />
+                                </div>
                             </div>
                             <div className="field">
                                 <label>Passwort</label>
-                                <input type="text" name="last-name" placeholder="pword"/>
+                                <div class="ui input">
+                                    <input type="text" placeholder="Passwort" />
+                                </div>
                             </div>
                             <button className="ui button" type="submit">Login</button>
                         </form>
                     </div>
-                    <div className={"three wide column"}>
-                        <img className="ui small image" src="./de4022dd39fa43041188aa8e937b6153.jpg"/>
+                    <div class="three wide column">
+                        <img src="/assets/images/wireframe/image.png" class="ui image" />
                     </div>
                 </div>
             </div>
-            </Layout>
         );
     }
 }
