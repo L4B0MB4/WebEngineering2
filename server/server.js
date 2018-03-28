25152f21e24874c3c332507c62d2493fdf5ff15f
L4B0MB4 <bommersbach1996@gmail.com>
2018-03-28 15:47:17 +0200

    Uploading Image with fetch

--- server/server.js
+++ server/server.js
@@ -218,7 +218,7 @@ app
             res.json(user);
         });
 
-        exp.post("/upload", function(req, res) {
+        exp.post("/api/uploadPicture", function(req, res) {
             if (!req.files) return res.status(400).send("No files were uploaded.");
 
             // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
