88ee820c804e80224310cad51ce9a209265cea5a
Felix Waldbach <felix-waldbach@t-online.de>
2018-04-02 12:48:03 +0200

    Added info texts to the About us page

--- components/kontakt.jsx
+++ components/kontakt.jsx
@@ -21,7 +21,7 @@ class Kontakt extends Component {
                         <Image src='../static/felix.jpg' />
                         <Card.Content>
                             <Card.Header>Prof. Dr. Med. Felix Waldbach</Card.Header>
-                            <Card.Meta>Backend of Excellence Lead Engineer North America</Card.Meta>
+                            <Card.Meta>Backend of Excellence Lead Engineer North America and NBA Commissioner</Card.Meta>
                             <Card.Description>Dr. Waldbach is the leading Backend Engineer for all departments in North America. 
                                 He graduated in Applied Medicine and Super Human Science in 2012 but decided to switch to something bigger. 
                                 Today most people know him for his excellent contribution to global web applications like golddigger.io, wasgehtdigger.io and aufsmauldigga.io.</Card.Description>
@@ -33,7 +33,10 @@ class Kontakt extends Component {
                         <Card.Content>
                             <Card.Header>Isabel Staaden</Card.Header>
                             <Card.Meta>Frontend Makeup Specialist & Worldwide Women Ambassador</Card.Meta>
-                            <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                            <Card.Description>Isabel was already one of the biggest players in IT business when everything started in the Silicon Valley in 1951.
+                                The fact that you can't tell her age by her looks (compared to her old friend and apprentice Bill Gates) says everything about her Makeup skills.
+                                Because of her passion for gold chains, shoes, teeth and even golden food everyone who was involved in the deal knew that she would be the right one
+                                to finally make golddigger.io unique.</Card.Description>
                         </Card.Content>
                     </Card>
 
@@ -41,8 +44,11 @@ class Kontakt extends Component {
                         <Image src='../static/bild.jpeg' />
                         <Card.Content>
                             <Card.Header>Lars Bommersbach</Card.Header>
-                            <Card.Meta>Joined in 2016</Card.Meta>
-                            <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                            <Card.Meta>CEO, Leading Tech Specialist & Blockchain Inventor and Innovator</Card.Meta>
+                            <Card.Description>In this case the title says it all: You can't spell Blockchain without Lars Bommersbach. The two time NFL MVP and leading hammer throw world record holder
+                                changed the whole business world with his new and completely different, decentralized finance and transaction mechanism. Because this is not enough he founded golddigger.io
+                                and while ruining every world-leading global bank he is also changing the game of Social Media right now. Thanks to this man the future looks bright. Oh, and just in case you
+                                are attempting to steal his loved motorcycle: Don't attempt it unless you can hack RSA encryption.</Card.Description>
                         </Card.Content>
                     </Card>
 
@@ -50,8 +56,10 @@ class Kontakt extends Component {
                         <Image src='../static/tino.jpg' />
                         <Card.Content>
                             <Card.Header>Tino Metzger</Card.Header>
-                            <Card.Meta>Joined in 2016</Card.Meta>
-                            <Card.Description>Daniel is a comedian living in Nashville.</Card.Description>
+                            <Card.Meta>Incarnate hyperbrain in Frontend Chief Development & Anti Greenhouse Effect Activist </Card.Meta>
+                            <Card.Description>Even though Tino just joint the golddigger.io association on 4/20 you can already see his massive impact on the unique and revolutionary frontend design and also the
+                                user count. Because he has connections to Greenhouse people from all over the world he was able to create a massive fanbase for this organization.
+                                So whenever you dig some gold here just remember that this OG mbzy made all of this possible.</Card.Description>
                         </Card.Content>
                     </Card>
                 </Card.Group>
