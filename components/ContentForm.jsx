429a6a603184843db57ee64657c8b6a2972d8fa3
L4B0MB4 <metalfan65@gmail.com>
2018-04-06 09:16:39 +0200

    Clearing image input after upload

--- components/ContentForm.jsx
+++ components/ContentForm.jsx
@@ -2,69 +2,69 @@ import React, { Component } from "react";
 import { Button, Checkbox, Form, TextArea, Input } from "semantic-ui-react";
 
 export default class ContentForm extends Component {
-    state = {
-        content: " "
-    };
+  state = {
+    content: " "
+  };
 
-    onSuccessFullyPosted = () => {
-        this.setState({ buttonSucess: true, buttonLoading: false });
-        setInterval(() => this.setState({ buttonSucess: false }), 1000);
-    };
+  onSuccessFullyPosted = () => {
+    this.state.inputImage.value = "";
+    this.setState({ content: "", buttonSucess: true, buttonLoading: false, file: undefined });
+    setInterval(() => this.setState({ buttonSucess: false }), 1000);
+  };
 
-    sendContent = async () => {
-        if (this.state.content.length > 0 && !this.state.file) {
-            this.props.blockchainWrapper.newTransaction("content", { text: this.state.content }, this.onSuccessFullyPosted);
-            this.setState({ buttonLoading: true });
-        } else if (this.state.file) {
-            this.setState({ buttonLoading: true });
-            const { data } = await this.props.request.callUploadFile(this.state.file);
-            if (data.filename) {
-                this.props.blockchainWrapper.newTransaction(
-                    "content",
-                    { text: this.state.content, picture: data.filename },
-                    this.onSuccessFullyPosted
-                );
-            }
-        }
-        this.setState({ content: "" });
-    };
-
-    isLoading = () => {
-        return this.state.buttonLoading;
-    };
-    isSuccessfull = () => {
-        return !this.state.buttonLoading && this.state.buttonSucess;
-    };
-
-    onSelectFiles = (files) => {
-        this.setState({ file: files[0] });
-    };
-
-    render() {
-        return (
-            <Form>
-                <Form.Field>
-                    <label>Post something!</label>
-                    <TextArea
-                        placeholder="Content"
-                        value={this.isSuccessfull() ? "" : undefined}
-                        onChange={(e) => this.setState({ content: e.target.value })}
-                    />
-                </Form.Field>
-                <Form.Field>
-                    <Input type="file" onChange={(e) => this.onSelectFiles(e.target.files)} className="upload" />
-                </Form.Field>
-                <Form.Field className="-text-right">
-                    <Button
-                        type="submit"
-                        loading={this.isLoading()}
-                        color={this.isSuccessfull() ? "green" : null}
-                        onClick={this.isLoading() ? null : this.sendContent}
-                    >
-                        {this.isSuccessfull() ? "Success" : "Post"}
-                    </Button>
-                </Form.Field>
-            </Form>
+  sendContent = async () => {
+    if (this.state.content.length > 0 && !this.state.file) {
+      this.props.blockchainWrapper.newTransaction("content", { text: this.state.content }, this.onSuccessFullyPosted);
+      this.setState({ buttonLoading: true });
+    } else if (this.state.file) {
+      this.setState({ buttonLoading: true });
+      const { data } = await this.props.request.callUploadFile(this.state.file);
+      if (data.filename) {
+        this.props.blockchainWrapper.newTransaction(
+          "content",
+          { text: this.state.content, picture: data.filename },
+          this.onSuccessFullyPosted
         );
+      }
     }
+    this.setState({ content: "" });
+  };
+
+  isLoading = () => {
+    return this.state.buttonLoading;
+  };
+  isSuccessfull = () => {
+    return !this.state.buttonLoading && this.state.buttonSucess;
+  };
+
+  onSelectFiles = e => {
+    this.setState({ inputImage: e.target, file: e.target.files[0] });
+  };
+
+  render() {
+    return (
+      <Form>
+        <Form.Field>
+          <label>Post something!</label>
+          <TextArea
+            placeholder="Content"
+            value={this.isSuccessfull() ? "" : undefined}
+            onChange={e => this.setState({ content: e.target.value })}
+          />
+        </Form.Field>
+        <Form.Field>
+          <Input type="file" onChange={e => this.onSelectFiles(e)} className="upload" />
+        </Form.Field>
+        <Form.Field className="-text-right">
+          <Button
+            type="submit"
+            loading={this.isLoading()}
+            color={this.isSuccessfull() ? "green" : null}
+            onClick={this.isLoading() ? null : this.sendContent}>
+            {this.isSuccessfull() ? "Success" : "Post"}
+          </Button>
+        </Form.Field>
+      </Form>
+    );
+  }
 }
