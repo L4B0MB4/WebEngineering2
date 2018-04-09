f84b9eba4be81de95552e3417c88f463ed2afd50
L4B0MB4 <metalfan65@gmail.com>
2018-04-09 22:03:20 +0200

    message if no news

--- components/News.jsx
+++ components/News.jsx
@@ -5,24 +5,32 @@ export default class News extends Component {
   render() {
     let i = 0;
     const { news } = this.props;
-    if (!news) return "";
+    if (!news) return "No news currently";
     return (
       <List>
-        {news.map(item => {
-          i++;
-          return (
-            <List.Item style={{ minWidth: "200px" }} key={i}>
-              <Image
-                src={item.user && item.user.profilePicture ? "/api/picture/" + item.user.profilePicture : "../static/bild.jpeg"}
-                avatar
-              />
-              <List.Content>
-                <List.Header as="a">{item.user ? item.user.name : "someone"}</List.Header>
-                <List.Description>{item.type}d your content</List.Description>
-              </List.Content>
-            </List.Item>
-          );
-        })}
+        {news ? (
+          news.map(item => {
+            i++;
+            return (
+              <List.Item style={{ minWidth: "200px" }} key={i}>
+                <Image
+                  src={item.user && item.user.profilePicture ? "/api/picture/" + item.user.profilePicture : "../static/bild.jpeg"}
+                  avatar
+                />
+                <List.Content>
+                  <List.Header as="a">{item.user ? item.user.name : "someone"}</List.Header>
+                  <List.Description>{item.type}d your content</List.Description>
+                </List.Content>
+              </List.Item>
+            );
+          })
+        ) : (
+          <List.Item>
+            <List.Content>
+              <List.Header>No news currently</List.Header>
+            </List.Content>
+          </List.Item>
+        )}
       </List>
     );
   }
